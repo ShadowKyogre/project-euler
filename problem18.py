1096d0766f8c0a0e10aa3cb6c06470fe91393a5e
ShadowKyogre <shadowkyogre.public@gmail.com>
2013-08-06 10:15:59 -0700

    Create problem18.py

---
+++ problem18.py
@@ -0,0 +1,26 @@
+from copy import deepcopy
+TRIANGLE=[[75],
+[95, 64],
+[17, 47, 82],
+[18, 35, 87, 10],
+[20, 4, 82, 47, 65],
+[19, 1, 23, 75, 3, 34],
+[88, 2, 77, 73, 7, 63, 67],
+[99, 65, 4, 28, 6, 16, 70, 92],
+[41, 41, 26, 56, 83, 40, 80, 70, 33],
+[41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+[53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+[70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+[91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+[63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+[4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],]
+
+def traverseTriangle(target):
+  rown = len(target)
+  largestVals = deepcopy(TRIANGLE[-1])
+  for i in range(rown-2, -1, -1):
+    for j in range(i+1):
+      largestVals[j]=target[i][j]+max(largestVals[j],largestVals[j+1])
+  return largestVals[0]
+
+print(traverseTriangle(TRIANGLE))
